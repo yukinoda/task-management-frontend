2dc9ba8e3311e2c8fb2b6942d2e91d66c6fc1f63
yukinoda <yukinoda@users.noreply.github.com>
2022-02-01 06:39:06 +0000

    deploy: 0c55014033045c04fdf430c04a56551a3701a4b6

---
+++ precache-manifest.fe29ab360671423d541b1d347d1717a0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d8df7b1f8de790437093b5b155d27c2",
+    "url": "https://yukinoda.github.io/task-management-frontend/index.html"
+  },
+  {
+    "revision": "13d676ff78fb1b572088",
+    "url": "https://yukinoda.github.io/task-management-frontend/static/css/main.5ac26d30.chunk.css"
+  },
+  {
+    "revision": "94b9837e5fa426dafd22",
+    "url": "https://yukinoda.github.io/task-management-frontend/static/js/2.58de6d96.chunk.js"
+  },
+  {
+    "revision": "13d676ff78fb1b572088",
+    "url": "https://yukinoda.github.io/task-management-frontend/static/js/main.cdbb924b.chunk.js"
+  },
+  {
+    "revision": "fd4e095018f9b91301ab",
+    "url": "https://yukinoda.github.io/task-management-frontend/static/js/runtime~main.03d56687.js"
+  }
+]);
